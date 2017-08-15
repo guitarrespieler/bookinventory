15c5ee5fcf5ed2f96f0380d792e68f8ca72cdcae
guitarrespieler <tibor.zsiga@hotmail.com>
2017-08-15 22:42:14 +0200

    new export class

---
+++ inventory/src/main/java/etc/Export.java
@@ -0,0 +1,12 @@
+package etc;
+
+import model.Inventory;
+
+public class Export {
+	private static final String xlsxExt = ".xlsx";
+	private static final String exportFileName = "könyvgyűjtemény" + xlsxExt;
+	
+	public static void exportInventory(Inventory inventory){
+		
+	}
+}

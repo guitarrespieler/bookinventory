cbf112dda675ca02b7ee2ea46090d09209330350
guitarrespieler <tibor.zsiga@hotmail.com>
2017-08-18 15:43:06 +0200

    ezaz

--- inventory/src/main/java/application/newBookScreenController.java
+++ inventory/src/main/java/application/newBookScreenController.java
@@ -2,6 +2,62 @@ package application;
 
 import javafx.fxml.FXML;
 
+import javafx.scene.control.Button;
+
+import javafx.scene.control.TextField;
+
+import javafx.scene.control.Label;
+
+import javafx.scene.control.RadioButton;
+
+import javafx.scene.control.DatePicker;
+
 public class newBookScreenController {
+	@FXML
+	private TextField titleTF;
+	@FXML
+	private TextField authorTF;
+	@FXML
+	private TextField PublicationTypeTF;
+	@FXML
+	private Button addAuthorBtn;
+	@FXML
+	private TextField categoryTF;
+	@FXML
+	private Button addPubTypeBtn;
+	@FXML
+	private TextField publisherTF;
+	@FXML
+	private DatePicker dateOfPublish;
+	@FXML
+	private Button saveBtn;
+	@FXML
+	private TextField roomTF;
+	@FXML
+	private TextField caseTF;
+	@FXML
+	private TextField shelfTF;
+	@FXML
+	private Button addCategoryBtn;
+	@FXML
+	private RadioButton lendableCB;
+	@FXML
+	private Button addSubPublicationBtn;
+	@FXML
+	private Label titleLbl;
+	@FXML
+	private Label authorLbl;
+	@FXML
+	private Label publisherLbl;
+	@FXML
+	private Label dateOfPublishLbl;
+	@FXML
+	private Label categoryLbl;
+	@FXML
+	private Label pubTypeLbl;
+	@FXML
+	private Label placeLbl;
+	@FXML
+	private Label subPubsLbl;
 
 }

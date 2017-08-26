38535b6061823dc6103d248d0f390b73d6bc837c
guitarrespieler <tibor.zsiga@hotmail.com>
2017-08-26 18:06:12 +0200

    kivettem egy felesleges importot

--- inventory/src/main/java/application/newBookScreenController.java
+++ inventory/src/main/java/application/newBookScreenController.java
@@ -7,7 +7,6 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.Locale;
 import java.util.ResourceBundle;
 import java.util.Set;
 
